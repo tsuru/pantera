d86cdcb74e61bc1f20c32de28dbd9e368f4e9876
Andrews Medina <andrewsmedina@gmail.com>
2013-01-02 17:17:46 -0200

    refactored methods to use connect

--- tests/test_choice_vm.py
+++ tests/test_choice_vm.py
@@ -4,11 +4,8 @@ import mock
 
 class ChooseVmTestCase(unittest.TestCase):
     def test_choice_vm(self):
-        vms = ["1", "2"]
         expected = "1"
-        with mock.patch("boto.ec2.connection.EC2Connection") as ec2con:
-            conn = ec2con.return_value
-            conn.terminate_instances.return_value = vms
+        with mock.patch("pantera.caos.connect") as conn:
             with mock.patch("random.choice") as choice:
                 from pantera import caos
                 choice.return_value = expected
--- tests/test_remove_vm.py
+++ tests/test_remove_vm.py
@@ -2,13 +2,13 @@ import unittest
 import mock
 
 
-
 class RemoveVmTestCase(unittest.TestCase):
     def test_remove_vm(self):
         vms = ["1", "2"]
-        with mock.patch("boto.ec2.connection.EC2Connection") as ec2con:
+        with mock.patch("pantera.caos.connect") as conn:
+            instance = mock.Mock()
+            instance.terminate_instances.return_value = vms
+            conn.return_value = instance
             from pantera import caos
-            conn= ec2con.return_value
-            conn.terminate_instances.return_value = vms
             destroyed = caos.destroy_vm()
         self.assertListEqual(vms, destroyed)
