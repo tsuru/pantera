de7350bde0117f574d334ff9fe6f346c468c24db
Andrews Medina <andrewsmedina@gmail.com>
2013-01-03 16:57:41 -0200

    Added version and fixed pep8.

--- pantera/__init__.py
+++ pantera/__init__.py
@@ -1,6 +1,10 @@
 import chaos
 import random
 
+
+__version__ = "0.1"
+
+
 def run(*args, **kwargs):
-  run_list = [chaos.Stop, chaos.Reboot, chaos.Terminate]
-  random.choice(run_list)(*args, **kwargs)()
+    run_list = [chaos.Stop, chaos.Reboot, chaos.Terminate]
+    random.choice(run_list)(*args, **kwargs)()
