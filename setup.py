5ca62c5e4d863c7a59ea6bdaba8584aff24fc49b
Francisco Souza <fss@corp.globo.com>
2013-01-21 10:31:01 -0200

    setup: fix flake8 issues

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ from pantera import __version__
 
 with open('requirements.txt') as reqs:
     install_requires = [
-        line for line in reqs.read().split('\n') if (line and not line.startswith('--'))
+        line for line in reqs.read().split('\n')
+        if (line and not line.startswith('--'))
     ]
 
 
